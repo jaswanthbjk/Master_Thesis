d47be2b8fa71f27d01f24adeb6827bf2ccd52053
jaswanthbjk <>
2021-06-17 11:59:13 +0200

    Adding yolo detection branch

--- models/yolo_v3/model.py
+++ models/yolo_v3/model.py
@@ -96,3 +96,50 @@ def yolo_neck(inputs, filters, training, data_format):
                         data_format=data_format, training=training)
 
     return route, inputs
+
+
+def yolo_detection_branch(inputs, number_of_classes, anchors, image_size,
+                          data_format):
+    num_anchors = len(anchors)
+
+    inputs = tf.keras.layers.Conv2D(filters=num_anchors*(5+number_of_classes),
+                                    kernel_size=1, strides=1, use_bias=True,
+                                    data_format=data_format)(inputs)
+
+    shape = inputs.get_shape().as_list()
+
+    if data_format == 'channels_first':
+        grid_shape = shape[2:4]
+        inputs = tf.transpose(inputs, [0, 2, 3, 1])
+    else:
+        grid_shape = shape[1:3]
+
+    inputs = tf.reshape(inputs, [-1,
+                                 num_anchors * grid_shape[0] * grid_shape[1],
+                                 5 + number_of_classes])
+    strides = (image_size[0] // grid_shape[0], image_size[1] // grid_shape[1])
+
+    box_centers, box_shapes, confidence, classes = \
+        tf.split(inputs, [2, 2, 1, number_of_classes], axis=-1)
+
+    x = tf.range(grid_shape[0], dtype=tf.float32)
+    y = tf.range(grid_shape[1], dtype=tf.float32)
+    x_offset, y_offset = tf.meshgrid(x, y)
+    x_offset = tf.reshape(x_offset, (-1, 1))
+    y_offset = tf.reshape(y_offset, (-1, 1))
+    x_y_offset = tf.concat([x_offset, y_offset], axis=-1)
+    x_y_offset = tf.tile(x_y_offset, [1, num_anchors])
+    x_y_offset = tf.reshape(x_y_offset, [1, -1, 2])
+    box_centers = tf.nn.sigmoid(box_centers)
+    box_centers = (box_centers + x_y_offset) * strides
+
+    anchors = tf.tile(anchors, [grid_shape[0] * grid_shape[1], 1])
+    box_shapes = tf.exp(box_shapes) * tf.cast(anchors, dtype=tf.float32)
+
+    confidence = tf.nn.sigmoid(confidence)
+
+    classes = tf.nn.sigmoid(classes)
+
+    detections = tf.concat([box_centers, box_shapes, confidence, classes], axis=-1)
+
+    return detections
