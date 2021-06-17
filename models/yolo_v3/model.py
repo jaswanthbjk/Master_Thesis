546c231e3be8d8572d273bce6c1cbb232012dbce
jaswanthbjk <>
2021-06-17 19:11:15 +0200

    Adding the final and completed yolo model

--- models/yolo_v3/model.py
+++ models/yolo_v3/model.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
-from utils import conv_padding
+from utils import conv_padding, build_boxes, non_maximum_suppression
+from model_parameters import _ANCHORS as anchors
 
 
 def conv_block(inputs, filters, kernel_size, strides=1, training=True,
@@ -76,10 +77,27 @@ def darknet53(inputs, training, data_format):
     return route_1, route_2, route_3
 
 
+def Upsampling_layer(inputs, output_shape, data_format):
+    if data_format == 'channels_first':
+        inputs = tf.transpose(inputs, [0, 2, 3, 1])
+        new_height = output_shape[3]
+        new_width = output_shape[2]
+    else:
+        new_height = output_shape[2]
+        new_width = output_shape[1]
+
+    inputs = tf.image.resize(inputs, [new_height, new_width], method='nearest')
+
+    if data_format == 'channels_first':
+        inputs = tf.transpose(inputs, [0, 3, 1, 2])
+
+    return inputs
+
+
 def yolo_neck(inputs, filters, training, data_format):
     inputs = conv_block(inputs, filters=filters, kernel_size=1,
                         data_format=data_format, training=training)
-    inputs = conv_block(inputs, filters=2*filters, kernel_size=3,
+    inputs = conv_block(inputs, filters=2 * filters, kernel_size=3,
                         data_format=data_format, training=training)
 
     inputs = conv_block(inputs, filters=filters, kernel_size=1,
@@ -102,9 +120,10 @@ def yolo_detection_branch(inputs, number_of_classes, anchors, image_size,
                           data_format):
     num_anchors = len(anchors)
 
-    inputs = tf.keras.layers.Conv2D(filters=num_anchors*(5+number_of_classes),
-                                    kernel_size=1, strides=1, use_bias=True,
-                                    data_format=data_format)(inputs)
+    inputs = tf.keras.layers.Conv2D(
+        filters=num_anchors * (5 + number_of_classes),
+        kernel_size=1, strides=1, use_bias=True,
+        data_format=data_format)(inputs)
 
     shape = inputs.get_shape().as_list()
 
@@ -144,3 +163,89 @@ def yolo_detection_branch(inputs, number_of_classes, anchors, image_size,
                            axis=-1)
 
     return detections
+
+
+class YoloV3:
+    def __init__(self, num_classes, input_size, output_size, iou_threshold,
+                 objectness_threshold, data_format=None):
+        if not data_format:
+            if tf.test.is_built_with_cuda():
+                self.data_format = 'channels_first'
+            else:
+                self.data_format = 'channels_last'
+        self.num_classes = num_classes
+        self.input_size = input_size
+        self.output_size = output_size
+        self.iou_threshold = iou_threshold
+        self.objectness_threshold = objectness_threshold
+
+    def __call__(self, images, training):
+        with tf.compat.v1.variable_scope('yolov3_model'):
+            if self.data_format == 'channels_first':
+                images = tf.transpose(images, [0, 3, 1, 2])
+
+            images = images / 255
+
+            route_1, route_2, route_3 = darknet53(images, training,
+                                                  data_format=self.data_format)
+
+            route, inputs = yolo_neck(route_3, filters=512, training=training,
+                                      data_format=self.data_format)
+
+            detection_scale_1 = yolo_detection_branch(inputs, self.num_classes,
+                                                      anchors[6:9],
+                                                      self.input_size,
+                                                      self.data_format)
+
+            inputs = conv_block(inputs, filters=256, kernel_size=1,
+                                training=training, data_format=self.data_format)
+
+            upsample_size = route_2.get_shape().as_list()
+
+            inputs = Upsampling_layer(inputs, output_shape=upsample_size,
+                                      data_format=self.data_format)
+
+            axis = 1 if self.data_format == 'channels_first' else 3
+
+            inputs = tf.concat([inputs, route_2], axis=axis)
+
+            route, inputs = yolo_neck(inputs, filters=256, training=training,
+                                      data_format=self.data_format)
+
+            detection_scale_2 = yolo_detection_branch(inputs, self.num_classes,
+                                                      anchors[6:9],
+                                                      self.input_size,
+                                                      self.data_format)
+
+            inputs = conv_block(route, filters=128, kernel_size=1,
+                                training=training, data_format=self.data_format)
+
+            upsample_size = route_1.get_shape().as_list()
+
+            inputs = Upsampling_layer(inputs, output_shape=upsample_size,
+                                      data_format=self.data_format)
+
+            inputs = tf.concat([inputs, route_1], axis=axis)
+
+            route, inputs = yolo_neck(inputs, filters=256, training=training,
+                                      data_format=self.data_format)
+
+            detection_scale_3 = yolo_detection_branch(inputs, self.num_classes,
+                                                      anchors[6:9],
+                                                      self.input_size,
+                                                      self.data_format)
+
+            inputs = tf.concat([detection_scale_1, detection_scale_2,
+                                detection_scale_3], axis=1)
+
+            inputs = build_boxes(inputs)
+
+            boxes_dicts = non_maximum_suppression(inputs,
+                                                  num_classes=self.num_classes,
+                                                  output_size=self.output_size,
+                                                  iou_threshold=
+                                                  self.iou_threshold,
+                                                  objectness_threshold=
+                                                  self.objectness_threshold)
+
+            return boxes_dicts
